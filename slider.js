7c6168e0d8727493ddf9abdf4d404d0528f9b2a8
Sofiaaa <sophia_forever@hotmai.com>
2019-09-02 01:35:08 +0300

    Add files via upload

--- slider.js
+++ slider.js
@@ -1,30 +1,13 @@
-
 let slides_parameters=[
     {
         img_url:'images/summer-1.jpg',
-        duration: 10,
+        duration: 5,
         title: {
             text: 'The sun and the sea',
             color: '#C90',
             bgcolor: '#000',
-            halign: 'right',   
-            valign: 'top',     
-            fontsize: '18px'
-        },
-        entry: {
-            fx: 'fade-in',      
-            duration: 10,
-        }
-    },
-    {
-        img_url:'images/summer-2.jpg',
-        duration: 5,
-        title: {
-            text: 'The beach',
-            color: '#C90',
-            bgcolor: '#000',
-            halign: 'left',    
-            valign: 'top',     
+            halign: 'right',    
+            valign: 'top',   
             fontsize: '18px'
         },
         entry: {
@@ -33,29 +16,13 @@ let slides_parameters=[
         }
     },
     {
-        img_url:'images/summer-3.jpg',
+        img_url:'images/summer-2.jpg',
         duration: 10,
         title: {
-            text: 'The ocean view',
+            text: 'The beach',
             color: '#C90',
             bgcolor: '#000',
-            halign: 'right',    
-            valign: 'top',     
-            fontsize: '18px'
-        },
-        entry: {
-            fx: 'fade-in',     
-            duration: 10,
-        }
-    },
-    {
-        img_url:'images/summer-4.jpg',
-        duration: 10,
-        title: {
-            text: 'waves',
-            color: '#C90',
-            bgcolor: '#000',
-            halign: 'right',    
+            halign: 'left',    
             valign: 'top',    
             fontsize: '18px'
         },
@@ -65,10 +32,10 @@ let slides_parameters=[
         }
     },
     {
-        img_url:'images/summer-5.jpg',
-        duration: 10,
+        img_url:'images/summer-3.jpg',
+        duration: 5,
         title: {
-            text: 'Tropical Island',
+            text: 'The ocean view',
             color: '#C90',
             bgcolor: '#000',
             halign: 'right',    
@@ -76,8 +43,40 @@ let slides_parameters=[
             fontsize: '18px'
         },
         entry: {
+            fx: 'fade-in',      
+            duration: 5,
+        }
+    },
+    {
+        img_url:'images/summer-4.jpg',
+        duration: 10,
+        title: {
+            text: 'waves',
+            color: '#C90',
+            bgcolor: '#000',
+            halign: 'right',   
+            valign: 'top',     
+            fontsize: '18px'
+        },
+        entry: {
             fx: 'fade-in',     
             duration: 10,
+        }
+    },
+    {
+        img_url:'images/summer-5.jpg',
+        duration: 5,
+        title: {
+            text: 'Tropical Island',
+            color: '#C90',
+            bgcolor: '#000',
+            halign: 'right',    
+            valign: 'top',    
+            fontsize: '18px'
+        },
+        entry: {
+            fx: 'fade-in',     
+            duration: 5,
         }
     }
 
@@ -88,11 +87,9 @@ Slider('sliderA',slides_parameters);
 
 function Slider( elementId , configurationObject ){
     let activeElement=document.querySelector('#'+ elementId + ' .slide');
-    activeElement.setAttribute("style",`  opacity: 0; visibility:hidden; `);
    
     for(let i=1; i<configurationObject.length; i++) {
         const clonos = activeElement.cloneNode(true);
-      
 
         const activeTitle=document.createElement("span");
         const myText=document.createTextNode(configurationObject[i].title.text);
@@ -104,7 +101,6 @@ function Slider( elementId , configurationObject ){
         
         document.getElementById(elementId).append(clonos);
     }
-    
 
     const activeTitle=document.createElement("span");
     const myText=document.createTextNode(configurationObject[0].title.text);
@@ -117,24 +113,27 @@ function Slider( elementId , configurationObject ){
     if (configurationObject[0].entry.fx=='fade-in'){
         activeElement.setAttribute("style",`background-image:url(${configurationObject[0].img_url}); transition:opacity ${configurationObject[0].entry.duration}s; opacity: 1; visibility:visible;` );
     }
-    
-    for(let i=1;i<configurationObject.length;i++){
-    
-        setTimeout(function (){
-         
-            let nextElement=activeElement.nextElementSibling;
-            if (!nextElement) {
-                nextElement = document.querySelector(`#${elementId} div:first-of-type`);
-            }
-      
-            activeElement.setAttribute("style","opacity:0; visibility:hidden; ");
-            activeElement=nextElement;
-            if (configurationObject[i].entry.fx=="fade-in"){
-                activeElement.setAttribute("style",` background-image:url(${configurationObject[i].img_url}); transition:opacity ${configurationObject[i].entry.duration}s; opacity: 1; visibility:visible;`);
-            }
-            
-        }, configurationObject[i-1].duration*1000 );
+ 
+    myIndex=0;
 
-     
+    delay();
+
+   function delay(){
+        var i;  
+        var x = document.getElementsByClassName("slide");
+         for (i = 0; i < x.length; i++) {
+             x[i].setAttribute("style","opacity: 0; visibility:hidden;"); 
+            }
+        if (myIndex == x.length) {myIndex = 0;} 
+        var dur=configurationObject[myIndex].duration;
+        myIndex++;
+        
+        x[myIndex-1].setAttribute("style",` background-image:url(${configurationObject[myIndex-1].img_url}); transition:opacity ${configurationObject[myIndex-1].entry.duration}s; opacity: 1; visibility:visible;`); 
+        setTimeout(delay,dur*1000);    
+    
     }
+        
+    
+     
+    
 }
